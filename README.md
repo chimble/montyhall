a06e1f960afc82b1bba4bb55b6e264abcca2fbc0
Cameron Moeller <cameron.moeller@gmail.com>
2016-11-09 17:02:06 -0500

    finished with normal.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # montyhall
+
+##FILES:
+##monty_hall.py : This is the main file to run for the tests.
+##README.md : You are here now... Continue reading...
+
+###This simulation will test different methods of solving the
+###Monty-Hall problem and return the success rates. Assuming you know the
+###rules of the game... The first method will test never switching doors.
+###For the first method, you can input the number of doors for the test.
+###For the second method, it will ALWAYS switch doors when offered the choice.
+###For the third method, it will flip a coin to decide whether to switch
+###or not switch. For the last two tests, the number of doors defaults to 3.
+###Afterwards, all three results will spit out.
--- monty_hall.py
+++ monty_hall.py
@@ -1,33 +1,51 @@
-#user pick door
-#eliminate one of two non picked doors
-#offer choice to swap
-#compare choice (orig or swap) to winning door
-#if same, successes+=1
-#run 999 more times
 import random
 
-def monty_hall_no_switch():
+
+def monty_hall_no_switch(num_doors):
+    x = (int(num_doors) - 1)
     successes = 0
-    for i in range(999):
-        door_list = [1, 2, 3]
-        winning_door = random.randint(1, 3)
-        user_index = random.randint(0, 2)
-        user_door = user_index + 1
-        #door choice is user_door + 1
-        del door_list[user_index]
-        #door choice removed from options
-        #doorlist has 2 things in it.
-        if user_door == winning_door:
+    for i in range(1000):
+        door_list = list(range(int(num_doors)))
+        winning_door = random.randint(0, x)
+        user_door = random.randint(0, x)
+        if door_list[user_door] == door_list[winning_door]:
             successes += 1
-            i += 1
         else:
-            i+= 1
-    print(successes/10)
+            continue
+    return(successes/10)
 
 
 def monty_hall_switch():
+    successes = 0
+    for i in range(1000):
+        winning_door = random.randint(1, 3)
+        user_door = random.randint(1, 3)
+        if user_door == winning_door:
+            continue
+        else:
+            successes += 1
+    return(successes/10)
 
 
+def fifty_fifty():
+    successes = 0
+    for i in range(1000):
+        winning_door = random.randint(1, 3)
+        user_door = random.randint(1, 3)
+        switch = random.randint(0, 1)
+        if switch:
+            if user_door == winning_door:
+                continue
+            else:
+                successes += 1
+        else:
+            if user_door == winning_door:
+                successes += 1
+            else:
+                continue
+    return(successes/10)
 
 
-monty_hall_no_switch()
+doors = input("number of doors? ")
+print("Never switch %: {} \nAlways switch %: {}\n50/50 Switch/No switch %: {}"
+      .format(monty_hall_no_switch(doors), monty_hall_switch(), fifty_fifty()))
